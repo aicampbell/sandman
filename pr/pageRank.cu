61d90551010de3ecb4fd08a6cd3721ba7040ff87
aidan <aidan@polaris.Home>
2018-03-31 17:48:57 +0100

    Working pr

--- pr/pageRank.cu
+++ pr/pageRank.cu
@@ -12,6 +12,14 @@
 int* d_vertices;
 int* d_edges;
 int* d_out_degrees;
+int* d_destinations;
+
+//current pr values
+double* d_x;
+//new pr values
+double* d_y;
+
+double* d_weight;
 
 int* d_num_vertices;
 int* d_num_edges;
@@ -23,37 +31,64 @@ int world_size;
 int blocks;
 int threads;
 
+__global__ void CUDA_INIT_PR_VALUES(double* d_x, int* d_num_vertices){
+    int id = threadIdx.x + blockIdx.x * blockDim.x;
 
-__global__ void CUDA_ITERATE_KERNEL(int* d_vertices, int* d_destinations, int* d_y, int* d_out_degrees, int* d_num_vertices){
+    d_x[id] = 1.0f / *d_num_vertices;
 
-    int d = 0.85;
+}
+
+//d_y is the output page rank
+//d_x is the old values
+//d_destinations is the edges
+//d_vertices is the sources
+__global__ void CUDA_ITERATE_KERNEL(int* d_vertices, int* d_destinations, double* d_x, double* d_y, int* d_out_degrees, int* d_num_vertices){
+
+    double d = 0.85;
     int i;
+    int s;
+    double sum = 0;
 
     //Set the thread id
     int id = threadIdx.x + blockIdx.x * blockDim.x;
 
     if(d_out_degrees[id] != 0){
         for(i = d_vertices[id]; i < d_vertices[id +1]; i++){
-            d_y[i] += d_vertices[id] / d_out_degrees[id];
+            s = d_destinations[i];
+
+            printf("s: %d\n", s);
+
+            // new result += previous values / number of out degrees
+            sum += d * d_x[s] / d_out_degrees[s];
+
+             //printf("x[%d]: %.5f\n", s, d_x[s]);
+             //printf("outDegree[%d]: %d\n", s, d_out_degrees[s]);
         }
+
+        sum += (1 - d) / *d_num_vertices;
+
+        d_y[id] = sum;
     }
-    d_y[id] = ((1 - d) / *d_num_vertices) + (d * d_y[id]);
 }
 
-__global__ void CUDA_WEIGHTS_KERNEL(float* d_weights, int d_weight, int* d_vertices, int d_num_vertices){
+__global__ void CUDA_WEIGHTS_KERNEL(double* d_y, double* d_weight, int* d_num_vertices){
+
+    printf("Weight %1f\n", *d_weight);
+
     int id = threadIdx.x + blockIdx.x * blockDim.x;
 
-    d_weights[id] += d_weight * d_vertices[id];
+    d_y[id] += *d_weight * (1.0f / *d_num_vertices);
+
 }
 
-__global__ void CUDA_SCALE_SWAP_KERNEL(float* d_x, float* d_y, int d_weight){
+__global__ void CUDA_SCALE_SWAP_KERNEL(double* d_x, double* d_y, double* d_weight){
     int id = threadIdx.x + blockIdx.x * blockDim.x;
 
-    d_x[id] = d_y[id] * d_weight;
-    d_y[id] = 0.f;
+    d_x[id] = *d_weight * d_y[id];
+    d_y[id] = 0;
 }
 
-void setup(int* vertices, int* destinations, int* out_degrees, int num_vertices, int num_edges){
+void setup(int* vertices, int* destinations, int* out_degrees, int num_vertices, int num_edges, double* x){
     cudaMalloc((void**)&d_vertices, sizeof(int) * num_vertices);
     cudaMemcpy(d_vertices, vertices, sizeof(int) * num_vertices, cudaMemcpyHostToDevice);
 
@@ -68,22 +103,33 @@ void setup(int* vertices, int* destinations, int* out_degrees, int num_vertices,
 
     cudaMalloc((void**)&d_num_edges, sizeof(int));
     cudaMemcpy(d_num_edges, &num_edges, sizeof(int), cudaMemcpyHostToDevice);
+
+    cudaMalloc((void**)&d_x, sizeof(double) * num_vertices);
+    cudaMemcpy(d_x, x, sizeof(double) * num_vertices, cudaMemcpyHostToDevice);
+
+    cudaMalloc((void**)&d_y, sizeof(double) * num_vertices);
+
+    cudaMalloc((void**)&d_weight, sizeof(double));
+
+    CUDA_INIT_PR_VALUES <<<blocks, threads>>> (d_x, d_num_vertices);
+    cudaMemcpy(x, d_x, sizeof(double) * num_vertices, cudaMemcpyDeviceToHost);
+
 }
 
-void iterate( int* y ){
+void iterate(double* x, double* y, int num_vertices){
 
+    cudaMemcpy(d_x, x, sizeof(double) * num_vertices, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_y, y, sizeof(double) * num_vertices, cudaMemcpyHostToDevice);
 
-    cudaMalloc((void**)&d_y, sizeof(int) * num_vertices);
-    cudaMemcpy(d_destinations, destinations, sizeof(int) * num_vertices, cudaMemcpyHostToDevice);
+    CUDA_ITERATE_KERNEL <<<blocks, threads >>> (d_vertices, d_destinations, d_x, d_y, d_out_degrees, d_num_vertices);
 
-
-
-    CUDA_ITERATE_KERNEL <<<blocks, threads >>> (d_vertices, d_destinations, d_y, d_out_degrees, d_num_vertices);
+    cudaMemcpy(x, d_x, sizeof(double) * num_vertices, cudaMemcpyDeviceToHost);
+    cudaMemcpy(y, d_y, sizeof(double) * num_vertices, cudaMemcpyDeviceToHost);
 
 }
 
-float sum(float* array, int length){
-    float sum = 0.f;
+float sum(double* array, int length){
+    double sum = 0.f;
     float err = 0.f;
     int i;
     for(i = 0; i < length; i++){
@@ -93,11 +139,13 @@ float sum(float* array, int length){
         err = tmp - sum;
         err += y;
     }
+    printf("SUM: %.5f\n", sum);
+    printf("ERR: %.5f\n", err);
     return sum;
 }
 
 //Calculate manhatten distance between input and output
-float normdiff(float* input, float* output, int length){
+float normdiff(double* input, double* output, int length){
     float d = 0.f;
     float err = 0.f;
     int i;
@@ -111,37 +159,88 @@ float normdiff(float* input, float* output, int length){
     return d;
 }
 
-void pageRank(int* vertices, int maxVertices, int* edges, int maxEdges, int* outDegrees){
+void pageRank(int* vertices, int num_vertices, int* destinations, int num_destinations, int* outDegrees){
 
-    int blocks = 1;
-    int threads = maxVertices;
+    blocks = 1;
+    threads = num_vertices;
 
     int maxIterations = 100;
     int iteration = 0;
-    float tol = 0.0000005;
-    float* y;
-    float delta = 2;
+    double tol = 0.0000005;
+    double* x = (double *) malloc(num_vertices * sizeof(double));
+    double* y = (double *) malloc(num_vertices * sizeof(double));
+    double delta = 2;
 
-    setup();
+    setup(vertices, destinations, outDegrees, num_vertices, num_destinations, x);
 
     while(iteration < maxIterations && delta > tol){
 
+        printf("Iteration: %d\n", iteration);
+
         //call iterations
-        iterate();
+        iterate(x, y, num_vertices);
 
-    //    //constants (1-d)v[i] added in separately.
-        float weight = 1.0f - sum(y, maxVertices); //ensure y[] sums to 1
-    //    CUDA_WEIGHTS_KERNEL();
+        printf("x[0] = %.5f\n", x[0]);
+        printf("x[1] = %.5f\n", x[1]);
+        printf("x[2] = %.5f\n", x[2]);
+        printf("x[3] = %.5f\n", x[3]);
 
-    //    delta = normdiff(x, y, n);
-    //    iteration++;
 
-    //    //rescale to unit length
-        //
-    //    CUDA_SCALE_SWAP_KERNEL(float* x, float* y, int w)
-    //}
+        //printf("WEIGHTS\n");
+
+        //constants (1-d)v[i] added in separately.
+        double weight = 1.0f - sum(y, num_vertices); //ensure y[] sums to 1
+        printf("Weight : %.5f\n", weight);
+
+        cudaMemcpy(d_weight, &weight, sizeof(double), cudaMemcpyHostToDevice);
+        CUDA_WEIGHTS_KERNEL<<<blocks, threads>>>(d_y, d_weight, d_num_vertices);
+        cudaMemcpy(y, d_y, sizeof(double) * num_vertices, cudaMemcpyDeviceToHost);
+
+        //printf("x[0] = %.5f\n", x[0]);
+        //printf("x[1] = %.5f\n", x[1]);
+        //printf("x[2] = %.5f\n", x[2]);
+        //printf("x[3] = %.5f\n", x[3]);
+
+        printf("y[0] = %.5f\n", y[0]);
+        printf("y[1] = %.5f\n", y[1]);
+        printf("y[2] = %.5f\n", y[2]);
+        printf("y[3] = %.5f\n", y[3]);
+
+        delta = normdiff(x, y, num_vertices);
+        printf("Delta: %1f\n", delta);
+
+        //rescale to unit length
+
+        weight = 1.0f / sum(y, num_vertices);
+        cudaMemcpy(d_weight, &weight, sizeof(double), cudaMemcpyHostToDevice);
+        CUDA_SCALE_SWAP_KERNEL<<<blocks, threads>>>(d_x, d_y, d_weight);
+        cudaMemcpy(x, d_x, sizeof(double) * num_vertices, cudaMemcpyDeviceToHost);
+        cudaMemcpy(y, d_y, sizeof(double) * num_vertices, cudaMemcpyDeviceToHost);
+
+        printf("After swap\n");
+
+        printf("x[0] = %.5f\n", x[0]);
+        printf("x[1] = %.5f\n", x[1]);
+        printf("x[2] = %.5f\n", x[2]);
+        printf("x[3] = %.5f\n", x[3]);
+
+        printf("y[0] = %.5f\n", y[0]);
+        printf("y[1] = %.5f\n", y[1]);
+        printf("y[2] = %.5f\n", y[2]);
+        printf("y[3] = %.5f\n", y[3]);
+
+        iteration++;
+    }
 
     if(delta > tol){
-        printf("No convergence");
+        printf("No convergence\n");
+    }
+    else{
+        printf("Convergence Mofo\n");
+
+        int i;
+        for(i =0; i < num_vertices; i++){
+            printf("x[%d] = %.5f\n", i, x[i]);
+        }
     }
 }
